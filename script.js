8ae0a69a3939b998cfa7f8d7b82f6caea0c6934d
abrodex007 <mit007mane@yandex.ru>
2024-12-27 12:46:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ function addTaskInDom(task, idTask, isNew = true,compl=false) {
   taskContainer.appendChild(div); // Добавляем задание в контейнер
   /* Если это создание задания, пушим и сохраняем в хранилище */
   if (isNew) {
-    console.log(idTask+" "+task+" "+compl);
+    console.log(idTask+" "+idNew+" "+task+" "+compl);
     taskList.push(taskBody); // Пушим задние
     saveLocal(); // Сохраняем
   }
